9544b572e87bf73cc63bf96d8136ca6d542af8f9
Jan Masangcay <jancdm@hotmail.com>
2019-05-06 20:31:56 -0700

    Connection test before creating a branch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <button onClick={this.props.onClick}>This button has been clicked {this.props.clicks} times.</button>
+      <button onClick={this.props.onClick}>This button has been clicked {this.props.clicks} times.</button> // This is the button
     );
   }
 }
