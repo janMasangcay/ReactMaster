a7ae6d62010a836774ebcf55c2a9554c50efeb33
Jan Masangcay <jancdm@hotmail.com>
2019-05-06 20:26:12 -0700

    Initial commit(restored)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <button onClick={this.props.onClick}>This button has been clicked {this.props.clicks} times.</button>
+    );
+  }
+}
+
+export default App;
